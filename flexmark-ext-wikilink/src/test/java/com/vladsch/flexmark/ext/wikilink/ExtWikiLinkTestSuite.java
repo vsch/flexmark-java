f6ae04098b682daf6a790a46c6df980479fb0782
Vladimir Schneider <vladimir.schneider@gmail.com>
2016-06-12 21:55:05 -0400

    add wiki extension, cannot work properly without new additions

---
+++ flexmark-ext-wikilink/src/test/java/com/vladsch/flexmark/ext/wikilink/ExtWikiLinkTestSuite.java
@@ -0,0 +1,11 @@
+package com.vladsch.flexmark.ext.wikilink;
+
+import org.junit.runners.Suite;
+
+@org.junit.runner.RunWith(Suite.class)
+@Suite.SuiteClasses({
+        WikiLinkFullSpecTest.class,
+        WikiLinkSpecTest.class,
+})
+public class ExtWikiLinkTestSuite {
+}
