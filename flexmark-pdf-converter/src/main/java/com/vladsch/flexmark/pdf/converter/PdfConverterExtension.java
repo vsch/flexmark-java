71cc595805220026695ec121eadce195df211aa3
niksw7 <niksw7@gmail.com>
2019-01-28 22:11:38 +0530

    Add password protection support (#306)
    * Add support for password-protected pdf along with other access Permission described here https://pdfbox.apache.org/docs/2.0.5/javadocs/org/apache/pdfbox/pdmodel/encryption/AccessPermission.html
    
    * Updated the documentation for java-doc
    
    * Clean up comments left

--- flexmark-pdf-converter/src/main/java/com/vladsch/flexmark/pdf/converter/PdfConverterExtension.java
+++ flexmark-pdf-converter/src/main/java/com/vladsch/flexmark/pdf/converter/PdfConverterExtension.java
@@ -3,9 +3,13 @@ package com.vladsch.flexmark.pdf.converter;
 import com.openhtmltopdf.DOMBuilder;
 import com.openhtmltopdf.bidi.support.ICUBidiReorderer;
 import com.openhtmltopdf.bidi.support.ICUBidiSplitter;
+import com.openhtmltopdf.outputdevice.helper.BaseRendererBuilder;
+import com.openhtmltopdf.pdfboxout.PdfBoxRenderer;
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import com.vladsch.flexmark.util.options.DataHolder;
 import com.vladsch.flexmark.util.options.DataKey;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.ProtectionPolicy;
 import org.jsoup.Jsoup;
 import org.w3c.dom.Document;
 
@@ -18,10 +22,10 @@ import java.io.OutputStream;
  * Extension for converting Markdown to PDF
  * <p>
  * After document is rendered pass the HTML result to
- * {@link #exportToPdf(OutputStream, String, String, DataHolder)}
- * or {@link #exportToPdf(String, String, String, DataHolder)}
- * or {@link #exportToPdf(String, String, String, PdfRendererBuilder.TextDirection)}
- * or {@link #exportToPdf(OutputStream, String, String, PdfRendererBuilder.TextDirection)}
+ * {@link #exportToPdf(OutputStream, String, String, DataHolder, ProtectionPolicy protectionPolicy)}
+ * or {@link #exportToPdf(String, String, String, DataHolder, ProtectionPolicy protectionPolicy)}
+ * or {@link #exportToPdf(String, String, String, PdfRendererBuilder.TextDirection, ProtectionPolicy protectionPolicy)}
+ * or {@link #exportToPdf(OutputStream, String, String, PdfRendererBuilder.TextDirection, ProtectionPolicy protectionPolicy)}
  * <p>
  * The parsed Markdown text is rendered to HTML then converted to PDF
  * </p>
@@ -29,50 +33,68 @@ import java.io.OutputStream;
 public class PdfConverterExtension {
     public static final DataKey<PdfRendererBuilder.TextDirection> DEFAULT_TEXT_DIRECTION = new DataKey<PdfRendererBuilder.TextDirection>("DEFAULT_TEXT_DIRECTION", (PdfRendererBuilder.TextDirection) null);
 
-    public static void exportToPdf(String out, String html, String url, DataHolder options) {
-        exportToPdf(out, html, url, options.get(DEFAULT_TEXT_DIRECTION));
+    public static void exportToPdf(String out, String html, String url, DataHolder options, ProtectionPolicy protectionPolicy) {
+        exportToPdf(out, html, url, options.get(DEFAULT_TEXT_DIRECTION), protectionPolicy);
     }
 
-    public static void exportToPdf(String out, String html, String url, final PdfRendererBuilder.TextDirection defaultTextDirection) {
+    public static void exportToPdf(String out, String html, String url, final PdfRendererBuilder.TextDirection defaultTextDirection, ProtectionPolicy protectionPolicy) {
         try {
             OutputStream os = new FileOutputStream(out);
-            exportToPdf(os, html, url, defaultTextDirection);
+            exportToPdf(os, html, url, defaultTextDirection, protectionPolicy);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static void exportToPdf(final OutputStream os, final String html, final String url, final DataHolder options) {
-        exportToPdf(os, html, url, options.get(DEFAULT_TEXT_DIRECTION));
+    public static void exportToPdf(final OutputStream os, final String html, final String url, final DataHolder options, ProtectionPolicy protectionPolicy) {
+        exportToPdf(os, html, url, options.get(DEFAULT_TEXT_DIRECTION), protectionPolicy);
     }
 
-    public static void exportToPdf(final OutputStream os, final String html, final String url, final PdfRendererBuilder.TextDirection defaultTextDirection) {
+    public static void exportToPdf(final OutputStream os, final String html, final String url, final PdfRendererBuilder.TextDirection defaultTextDirection, final ProtectionPolicy protectionPolicy) {
+        PdfBoxRenderer renderer = null;
         try {
             // There are more options on the builder than shown below.
             PdfRendererBuilder builder = new PdfRendererBuilder();
 
-            if (defaultTextDirection != null) {
-                builder.useUnicodeBidiSplitter(new ICUBidiSplitter.ICUBidiSplitterFactory());
-                builder.useUnicodeBidiReorderer(new ICUBidiReorderer());
-                builder.defaultTextDirection(defaultTextDirection); // OR RTL
-            }
+            handleTextDirection(defaultTextDirection, builder);
+            handleW3cDocument(html, url, builder);
 
-            org.jsoup.nodes.Document doc;
-            doc = Jsoup.parse(html);
-
-            Document dom = DOMBuilder.jsoup2DOM(doc);
-            builder.withW3cDocument(dom, url);
             builder.toStream(os);
-            builder.run();
+            renderer = builder.buildPdfRenderer();
+            PDDocument document = renderer.getPdfDocument();
+            if (protectionPolicy != null)
+                document.protect(protectionPolicy);
+
+            renderer.layout();
+            renderer.createPDF();
         } catch (Exception e) {
             e.printStackTrace();
             // LOG exception
         } finally {
             try {
+                if (renderer != null) {
+                    renderer.close();
+                }
                 os.close();
             } catch (IOException e) {
                 // swallow
             }
+        }
+    }
+
+    private static void handleW3cDocument(final String html, final String url, final PdfRendererBuilder builder) {
+        org.jsoup.nodes.Document doc;
+        doc = Jsoup.parse(html);
+
+        Document dom = DOMBuilder.jsoup2DOM(doc);
+        builder.withW3cDocument(dom, url);
+    }
+
+    private static void handleTextDirection(final BaseRendererBuilder.TextDirection defaultTextDirection, final PdfRendererBuilder builder) {
+        if (defaultTextDirection != null) {
+            builder.useUnicodeBidiSplitter(new ICUBidiSplitter.ICUBidiSplitterFactory());
+            builder.useUnicodeBidiReorderer(new ICUBidiReorderer());
+            builder.defaultTextDirection(defaultTextDirection); // OR RTL
         }
     }
 }
