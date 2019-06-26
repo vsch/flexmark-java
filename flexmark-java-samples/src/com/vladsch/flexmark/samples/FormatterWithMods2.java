970492ddaf5a969579049278f36444f67a833142
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-06-26 12:39:33 -0400

    add formatter with mods 2

---
+++ flexmark-java-samples/src/com/vladsch/flexmark/samples/FormatterWithMods2.java
@@ -0,0 +1,99 @@
+package com.vladsch.flexmark.samples;
+
+import com.vladsch.flexmark.ast.Image;
+import com.vladsch.flexmark.ast.Link;
+import com.vladsch.flexmark.ast.LinkNodeBase;
+import com.vladsch.flexmark.ast.Reference;
+import com.vladsch.flexmark.convert.html.FlexmarkHtmlParser;
+import com.vladsch.flexmark.formatter.Formatter;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.ast.NodeVisitor;
+import com.vladsch.flexmark.util.ast.VisitHandler;
+import com.vladsch.flexmark.util.ast.Visitor;
+import com.vladsch.flexmark.util.data.DataHolder;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import com.vladsch.flexmark.util.sequence.PrefixedSubSequence;
+import com.vladsch.flexmark.util.sequence.SegmentedSequence;
+
+import java.util.Arrays;
+
+public class FormatterWithMods2 {
+    static final DataHolder OPTIONS = new MutableDataSet();
+
+    static final MutableDataSet FORMAT_OPTIONS = new MutableDataSet();
+    static {
+        // copy extensions from Pegdown compatible to Formatting
+        FORMAT_OPTIONS.set(Parser.EXTENSIONS, OPTIONS.get(Parser.EXTENSIONS));
+    }
+
+    static final Parser PARSER = Parser.builder(OPTIONS).build();
+    static final Formatter RENDERER = Formatter.builder(FORMAT_OPTIONS).build();
+
+    static class LinkNodeVisitor {
+        NodeVisitor visitor = new NodeVisitor(
+                new VisitHandler<Link>(Link.class, new Visitor<Link>() {
+                    @Override
+                    public void visit(Link node) {
+                        LinkNodeVisitor.this.visit(node);
+                    }
+                }),
+                new VisitHandler<Reference>(Reference.class, new Visitor<Reference>() {
+                    @Override
+                    public void visit(Reference node) {
+                        LinkNodeVisitor.this.visit(node);
+                    }
+                }),
+                new VisitHandler<Image>(Image.class, new Visitor<Image>() {
+                    @Override
+                    public void visit(Image node) {
+                        LinkNodeVisitor.this.visit(node);
+                    }
+                })
+        );
+
+        public void replaceUrl(Node node) {
+            visitor.visit(node);
+        }
+
+        private void visit(Link node) {
+            visit((LinkNodeBase) node);
+        }
+
+        private void visit(Reference node) {
+            visit((LinkNodeBase) node);
+        }
+
+        private void visit(Image node) {
+            visit((LinkNodeBase) node);
+        }
+
+        private void visit(LinkNodeBase node) {
+            if (node.getPageRef().startsWith("/")) {
+                node.setUrlChars(PrefixedSubSequence.of("https:", node.getPageRef()));
+                node.setChars(SegmentedSequence.of(Arrays.asList(node.getSegmentsForChars())));
+            }
+        }
+    }
+
+    // use the PARSER to parse and RENDERER to parse pegdown indentation rules and render CommonMark
+    public static void main(String[] args) {
+        String html = "<img src=\"//img.alicdn.com/tfscom/TB1mR4xPpXXXXXvapXXXXXXXXXX.jpg\" >";
+        String markdown = FlexmarkHtmlParser.parse(html);
+
+        System.out.println("html\n");
+        System.out.println(html);
+
+        System.out.println("markdown\n");
+        System.out.println(markdown);
+
+        Node document = PARSER.parse(markdown);
+        LinkNodeVisitor visitor = new LinkNodeVisitor();
+        visitor.replaceUrl(document);
+
+        String commonmark = RENDERER.render(document);
+
+        System.out.println("\n\nCommonMark\n");
+        System.out.println(commonmark);
+    }
+}
