29dfda0672631046e50929cd5a8d4a5edc06637c
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-10-18 13:56:13 -0400

    interim commit

--- flexmark-formatter-test/src/main/java/com/vladsch/flexmark/formatter/test/ComboFormatterSpecTestBase.java
+++ flexmark-formatter-test/src/main/java/com/vladsch/flexmark/formatter/test/ComboFormatterSpecTestBase.java
@@ -19,54 +19,26 @@ import java.util.Map;
 public abstract class ComboFormatterSpecTestBase extends ComboSpecTestCase {
     static final boolean SKIP_IGNORED_TESTS = true;
 
-    private final Map<String, DataHolder> optionsMap = new HashMap<>();
-    private final DataHolder myDefaultOptions;
+    private static final DataHolder OPTIONS = new MutableDataSet()
+            .set(Parser.BLANK_LINES_IN_AST, true)
+            .set(Parser.HEADING_NO_ATX_SPACE, true)
+            .toImmutable();
 
-    public ComboFormatterSpecTestBase(SpecExample example, @Nullable DataHolder defaultOptions, @Nullable Map<String, DataHolder> optionsMap) {
-        super(example);
-
-        // standard options
-        DataHolder OPTIONS = new MutableDataSet()
-                .set(Parser.BLANK_LINES_IN_AST, true)
-                .set(Parser.HEADING_NO_ATX_SPACE, true)
-                .toImmutable();
-
-        myDefaultOptions = combineOptions(OPTIONS, defaultOptions);
-
+    private static final Map<String, DataHolder> optionsMap = new HashMap<>();
+    static {
         // add standard options
-        this.optionsMap.put("IGNORED", new MutableDataSet().set(TestUtils.IGNORE, SKIP_IGNORED_TESTS));
-        this.optionsMap.put("format-fixed-indent", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
-        this.optionsMap.put("parse-fixed-indent", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
-        this.optionsMap.put("format-github", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
-        this.optionsMap.put("parse-github", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
-        this.optionsMap.put("max-blank-lines-1", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 1));
-        this.optionsMap.put("max-blank-lines-2", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 2));
-        this.optionsMap.put("max-blank-lines-3", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 3));
-        this.optionsMap.put("no-tailing-blanks", new MutableDataSet().set(Formatter.MAX_TRAILING_BLANK_LINES, 0));
-
-        if (optionsMap != null) {
-            this.optionsMap.putAll(optionsMap);
-        }
+        optionsMap.put("IGNORED", new MutableDataSet().set(TestUtils.IGNORE, SKIP_IGNORED_TESTS));
+        optionsMap.put("format-fixed-indent", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
+        optionsMap.put("parse-fixed-indent", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
+        optionsMap.put("format-github", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
+        optionsMap.put("parse-github", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
+        optionsMap.put("max-blank-lines-1", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 1));
+        optionsMap.put("max-blank-lines-2", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 2));
+        optionsMap.put("max-blank-lines-3", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 3));
+        optionsMap.put("no-tailing-blanks", new MutableDataSet().set(Formatter.MAX_TRAILING_BLANK_LINES, 0));
     }
-
-    @Nullable
-    public static Map<String, DataHolder> mergeMaps(@Nullable Map<String, DataHolder> other, @Nullable Map<String, DataHolder> overrides) {
-        if (other != null && overrides != null) {
-            HashMap<String, DataHolder> map = new HashMap<>(other);
-            map.putAll(overrides);
-            return map;
-        } else if (other != null) {
-            return other;
-        } else {
-            return overrides;
-        }
-    }
-
-    @Nullable
-    @Override
-    final public DataHolder options(String option) {
-        if (option == null) return null;
-        return optionsMap.get(option);
+    public ComboFormatterSpecTestBase(@NotNull SpecExample example, @Nullable DataHolder... defaultOptions) {
+        super(example, optionsMap, dataHolders(OPTIONS, defaultOptions));
     }
 
     @Override
--- flexmark-formatter-test/src/main/java/com/vladsch/flexmark/formatter/test/ComboTranslationFormatterSpecTestBase.java
+++ flexmark-formatter-test/src/main/java/com/vladsch/flexmark/formatter/test/ComboTranslationFormatterSpecTestBase.java
@@ -34,48 +34,31 @@ public abstract class ComboTranslationFormatterSpecTestBase extends ComboSpecTes
     public static final DataKey<Boolean> DETAILS = new DataKey<>("DETAILS", SHOW_INTERMEDIATE);
     public static final DataKey<Boolean> AST_DETAILS = new DataKey<>("AST_DETAILS", SHOW_INTERMEDIATE_AST);
 
-    // spacer
-    private final Map<String, DataHolder> optionsMap = new HashMap<>();
-    private final DataHolder myDefaultOptions;
+    private static DataHolder OPTIONS = new MutableDataSet()
+            .set(Parser.BLANK_LINES_IN_AST, true)
+            .set(Parser.HTML_FOR_TRANSLATOR, true)
+            .set(Parser.PARSE_INNER_HTML_COMMENTS, true)
+            .set(Parser.HEADING_NO_ATX_SPACE, true)
+            .set(Formatter.MAX_TRAILING_BLANK_LINES, 0)
+            .toImmutable();
 
-    public ComboTranslationFormatterSpecTestBase(SpecExample example, @Nullable DataHolder defaultOptions, @Nullable Map<String, DataHolder> optionsMap) {
-        super(example);
-
-        // standard options
-        DataHolder OPTIONS = new MutableDataSet()
-                .set(Parser.BLANK_LINES_IN_AST, true)
-                .set(Parser.HTML_FOR_TRANSLATOR, true)
-                .set(Parser.PARSE_INNER_HTML_COMMENTS, true)
-                .set(Parser.HEADING_NO_ATX_SPACE, true)
-                .set(Formatter.MAX_TRAILING_BLANK_LINES, 0)
-//                .set(TestUtils.NO_FILE_EOL, false)
-                .toImmutable();
-
-        myDefaultOptions = combineOptions(OPTIONS, defaultOptions);
-
-        // add standard options
-        this.optionsMap.put("details", new MutableDataSet().set(DETAILS, true));
-        this.optionsMap.put("ast-details", new MutableDataSet().set(AST_DETAILS, true));
-        this.optionsMap.put("IGNORED", new MutableDataSet().set(TestUtils.IGNORE, SKIP_IGNORED_TESTS));
-        this.optionsMap.put("format-fixed-indent", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
-        this.optionsMap.put("parse-fixed-indent", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
-        this.optionsMap.put("format-github", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
-        this.optionsMap.put("parse-github", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
-        this.optionsMap.put("max-blank-lines-1", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 1));
-        this.optionsMap.put("max-blank-lines-2", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 2));
-        this.optionsMap.put("max-blank-lines-3", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 3));
-        this.optionsMap.put("no-tailing-blanks", new MutableDataSet().set(Formatter.MAX_TRAILING_BLANK_LINES, 0));
-
-        if (optionsMap != null) {
-            this.optionsMap.putAll(optionsMap);
-        }
+    private static final Map<String, DataHolder> optionsMap = new HashMap<>();
+    static {
+        optionsMap.put("details", new MutableDataSet().set(DETAILS, true));
+        optionsMap.put("ast-details", new MutableDataSet().set(AST_DETAILS, true));
+        optionsMap.put("IGNORED", new MutableDataSet().set(TestUtils.IGNORE, SKIP_IGNORED_TESTS));
+        optionsMap.put("format-fixed-indent", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
+        optionsMap.put("parse-fixed-indent", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.FIXED_INDENT));
+        optionsMap.put("format-github", new MutableDataSet().set(Formatter.FORMATTER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
+        optionsMap.put("parse-github", new MutableDataSet().set(Parser.PARSER_EMULATION_PROFILE, ParserEmulationProfile.GITHUB_DOC));
+        optionsMap.put("max-blank-lines-1", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 1));
+        optionsMap.put("max-blank-lines-2", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 2));
+        optionsMap.put("max-blank-lines-3", new MutableDataSet().set(Formatter.MAX_BLANK_LINES, 3));
+        optionsMap.put("no-tailing-blanks", new MutableDataSet().set(Formatter.MAX_TRAILING_BLANK_LINES, 0));
     }
 
-    @Nullable
-    @Override
-    final public DataHolder options(String option) {
-        if (option == null) return null;
-        return optionsMap.get(option);
+    public ComboTranslationFormatterSpecTestBase(@NotNull SpecExample example, @Nullable DataHolder... options) {
+        super(example, optionsMap, dataHolders(OPTIONS, options));
     }
 
     private Parser getParser(@Nullable DataHolder OPTIONS) {
