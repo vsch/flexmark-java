a889cbdc0d4a4c71afe721890ac1b7f3afc4ec1e
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-11-09 08:47:03 -0500

    interim commit

--- flexmark-util/src/main/java/com/vladsch/flexmark/util/sequence/SegmentedSequenceBuilder.java
+++ flexmark-util/src/main/java/com/vladsch/flexmark/util/sequence/SegmentedSequenceBuilder.java
@@ -1,6 +1,9 @@
 package com.vladsch.flexmark.util.sequence;
 
+inteimport org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A Builder for Segmented BasedSequences
@@ -19,6 +22,18 @@ public final class SegmentedSequenceBuilder {
         this.base = base;
     }
 
+    public SegmentedSequenceBuilder subContext() {
+        return new SegmentedSequenceBuilder(base);
+    }
+
+    public SegmentedSequenceBuilder append(SegmentedSequenceBuilder other) {
+        if (base != other.base) {
+            throw new IllegalArgumentException("append SegmentedSequenceBuilder called with other having different base: " + base.hashCode() + " got: " + other.base.hashCode());
+        }
+        segments.addAll(other.segments);
+        return this;
+    }
+
     public SegmentedSequenceBuilder append(CharSequence s) {
         if (s instanceof BasedSequence) return append((BasedSequence) s);
         else if (s.length() > 0) return append(s.toString());
@@ -26,7 +41,12 @@ public final class SegmentedSequenceBuilder {
     }
 
     public SegmentedSequenceBuilder append(BasedSequence s) {
-        segments.add(s);
+        if (s.isNotNull()) {
+            if (s.getBaseSequence() != base) {
+                throw new IllegalArgumentException("setLastSegment called with segment having different base: " + base.hashCode() + " got: " + s.getBaseSequence().hashCode());
+            }
+            segments.add(s);
+        }
         return this;
     }
 
@@ -50,6 +70,33 @@ public final class SegmentedSequenceBuilder {
 
     public BasedSequence[] toSegments() {
         return segments.toArray(BasedSequence.EMPTY_SEGMENTS);
+    }
+
+    public List<BasedSequence> getSegments() {
+        return segments;
+    }
+
+    public BasedSequence getLastSegment() {
+        return segments.size() == 0 ? BasedSequence.NULL : segments.get(segments.size() - 1);
+    }
+
+    public void setLastSegment(@NotNull BasedSequence s) {
+        if (segments.size() == 0) {
+            throw new IllegalStateException("setLastSegment called with no segments");
+        }
+
+        if (s.getBaseSequence() != base) {
+            throw new IllegalArgumentException("setLastSegment called with segment having different base: " + base.hashCode() + " got: " + s.getBaseSequence().hashCode());
+        }
+
+        if (segments.size() > 1) {
+            BasedSequence lastSegment = segments.get(segments.size() - 2);
+            if (lastSegment.getEndOffset() < s.getStartOffset()) {
+                throw new IllegalArgumentException("setLastSegment called with segment starting before previous endOffset: " + lastSegment.getEndOffset() + " segment startOffset: " + s.getStartOffset());
+            }
+        }
+
+        segments.set(segments.size() - 1, s);
     }
 
     public int length() {
