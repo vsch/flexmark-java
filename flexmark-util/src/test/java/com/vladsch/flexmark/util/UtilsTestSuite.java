adab9ed5b47144f296ddf7e92a2eaa7aa8a1e731
Vladimir Schneider <vladimir.schneider@gmail.com>
2020-01-03 07:01:47 -0500

    interim commit

--- flexmark-util/src/test/java/com/vladsch/flexmark/util/UtilsTestSuite.java
+++ flexmark-util/src/test/java/com/vladsch/flexmark/util/UtilsTestSuite.java
@@ -1,71 +1,26 @@
 package com.vladsch.flexmark.util;
 
-import com.vladsch.flexmark.util.ast.DocumentTest;
-import com.vladsch.flexmark.util.collection.*;
-import com.vladsch.flexmark.util.format.MarkdownParagraphTest;
-import com.vladsch.flexmark.util.format.TrackedOffsetListTest;
-import com.vladsch.flexmark.util.html.HtmlFormattingAppendableBaseTest;
-import com.vladsch.flexmark.util.html.LineAppendableImplTest;
-import com.vladsch.flexmark.util.html.ui.HtmlBuilderTest;
-import com.vladsch.flexmark.util.html.ui.HtmlHelpersTest;
-import com.vladsch.flexmark.util.options.AttributeTest;
-import com.vladsch.flexmark.util.options.AttributesTest;
-import com.vladsch.flexmark.util.options.MutableAttributeTest;
-import com.vladsch.flexmark.util.sequence.*;
-import com.vladsch.flexmark.util.sequence.builder.BasedSegmentBuilderTest;
-import com.vladsch.flexmark.util.sequence.builder.PlainSegmentBuilderTest;
-import com.vladsch.flexmark.util.sequence.builder.SequenceBuilderTest;
-import com.vladsch.flexmark.util.sequence.builder.tree.BasedOffsetTrackerTest;
-import com.vladsch.flexmark.util.sequence.builder.tree.SegmentOffsetTreeTest;
-import com.vladsch.flexmark.util.sequence.builder.tree.SegmentTest;
-import com.vladsch.flexmark.util.sequence.builder.tree.SegmentTreeTest;
-import com.vladsch.flexmark.util.sequence.managed.BaseSequenceEntryTest;
-import com.vladsch.flexmark.util.sequence.managed.BaseSequenceManagerTest;
+import com.vladsch.flexmark.util.ast.AstTestSuite;
+import com.vladsch.flexmark.util.collection.CollectionTestSuite;
+import com.vladsch.flexmark.util.format.FormatTestSuite;
+import com.vladsch.flexmark.util.html.HtmlTestSuite;
+import com.vladsch.flexmark.util.misc.MiscTestSuite;
+import com.vladsch.flexmark.util.options.OptionsTestSuite;
+import com.vladsch.flexmark.util.sequence.SequenceTestSuite;
+import com.vladsch.flexmark.util.visitor.VisitorTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        OrderedMapTest.class,
-        OrderedMultiMapTest.class,
-        OrderedSetTest.class,
-        ClassificationBagTest.class,
-        CharPredicateTest.class,
-        SequenceUtilsTest.class,
-        MaxAggregatorTest.class,
-        MinAggregatorTest.class,
-        MutableAttributeTest.class,
-        BoundedMaxAggregatorTest.class,
-        BoundedMinAggregatorTest.class,
-        UtilsTest.class,
-        ArrayUtilsTest.class,
-        BitFieldSetTest.class,
-        PlainSegmentBuilderTest.class,
-        MappedRichSequenceTest.class,
-        RepeatedSequenceTest.class,
-        BaseSequenceEntryTest.class,
-        MappedBasedSequenceTest.class,
-        PrefixedSubSequenceTest.class,
-        SegmentTest.class,
-        BasedSegmentBuilderTest.class,
-        SegmentTreeTest.class,
-        SequenceBuilderTest.class,
-        BasedSequenceFullImplTest.class,
-        BasedSequenceTreeImplTest.class,
-        SegmentOffsetTreeTest.class,
-        BasedOffsetTrackerTest.class,
-        TrackedOffsetListTest.class,
-        BaseSequenceManagerTest.class,
-        MarkdownParagraphTest.class,
-        TemplateUtilTest.class,
-        AttributesTest.class,
-        AttributeTest.class,
-        DocumentTest.class,
-        HtmlBuilderTest.class,
-        HtmlFormattingAppendableBaseTest.class,
-        HtmlHelpersTest.class,
-        LineAppendableImplTest.class,
-//        LineAppendableImplTest.class,
+        CollectionTestSuite.class,
+        MiscTestSuite.class,
+        AstTestSuite.class,
+        HtmlTestSuite.class,
+        FormatTestSuite.class,
+        OptionsTestSuite.class,
+        VisitorTestSuite.class,
+        SequenceTestSuite.class,
 })
 public class UtilsTestSuite {
 }
