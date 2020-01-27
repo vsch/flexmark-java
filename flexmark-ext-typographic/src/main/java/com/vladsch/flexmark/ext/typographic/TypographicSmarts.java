751cfc95009f04b00769d6c83b75d9b3c4ddbc28
Vladimir Schneider <vladimir.schneider@gmail.com>
2020-01-27 10:33:04 -0500

    handle unescaped and raw text collection in typographic smarts

--- flexmark-ext-typographic/src/main/java/com/vladsch/flexmark/ext/typographic/TypographicSmarts.java
+++ flexmark-ext-typographic/src/main/java/com/vladsch/flexmark/ext/typographic/TypographicSmarts.java
@@ -4,6 +4,8 @@ import com.vladsch.flexmark.util.ast.DoNotAttributeDecorate;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.ast.TypographicText;
 import com.vladsch.flexmark.util.sequence.BasedSequence;
+import com.vladsch.flexmark.util.sequence.Escaping;
+import com.vladsch.flexmark.util.sequence.ReplacedTextMapper;
 import com.vladsch.flexmark.util.sequence.builder.ISequenceBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -33,7 +35,13 @@ public class TypographicSmarts extends Node implements DoNotAttributeDecorate, T
 
     @Override
     public boolean collectText(ISequenceBuilder<? extends ISequenceBuilder<?, BasedSequence>, BasedSequence> out, int flags) {
-        out.append(getChars());
+        if (any(flags, F_NODE_TEXT)) {
+            out.append(getChars());
+        } else {
+            ReplacedTextMapper textMapper = new ReplacedTextMapper(getChars());
+            BasedSequence unescaped = Escaping.unescape(getChars(), textMapper);
+            out.append(unescaped);
+        }
         return false;
     }
 
