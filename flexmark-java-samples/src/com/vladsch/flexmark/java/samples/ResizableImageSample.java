8fe903b9290a5f3c2218c4a1c3e012a4f6774d38
sparksparrow <50149745+sparksparrow@users.noreply.github.com>
2022-01-19 18:16:45 +0300

    delete comments

--- flexmark-java-samples/src/com/vladsch/flexmark/java/samples/ResizableImageSample.java
+++ flexmark-java-samples/src/com/vladsch/flexmark/java/samples/ResizableImageSample.java
@@ -15,16 +15,11 @@ public class ResizableImageSample {
     public static void main(String[] args) {
         MutableDataSet options = new MutableDataSet();
 
-        // uncomment to set optional extensions
         options.set(Parser.EXTENSIONS, Arrays.asList(ResizableImageExtension.create()));
-
-        // uncomment to convert soft-breaks to hard breaks
-        // options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
 
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
-
-        // You can re-use parser and renderer instances
+        
         Node document = parser.parse("![Image_30x30](https://raw.githubusercontent.com/sparksparrow/Intelligent-mobile-apps/master/Relativepath/test.jpg =30x30)");
         String html = renderer.render(document);
         System.out.println(html);
