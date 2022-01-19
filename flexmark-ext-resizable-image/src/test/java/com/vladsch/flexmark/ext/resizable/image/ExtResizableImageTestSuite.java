10dd57ba45ea5b8d4483c22dca4189e3f72932f8
sparksparrow <50149745+sparksparrow@users.noreply.github.com>
2022-01-19 14:48:53 +0300

    some fixes test, but not last(

--- flexmark-ext-resizable-image/src/test/java/com/vladsch/flexmark/ext/resizable/image/ExtResizableImageTestSuite.java
+++ flexmark-ext-resizable-image/src/test/java/com/vladsch/flexmark/ext/resizable/image/ExtResizableImageTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ComboResizableImageSpecTest.class,
+        ComboResizableImageSpecTest.class
 })
 public class ExtResizableImageTestSuite {
 }
