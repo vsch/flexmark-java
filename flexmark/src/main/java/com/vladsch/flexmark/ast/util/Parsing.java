cc669b238a467bdc790fdc4574169aae1e78420c
Vladimir Schneider <vladimir.schneider@gmail.com>
2022-01-30 08:53:25 -0500

    fix namespace regex for efficiency

--- flexmark/src/main/java/com/vladsch/flexmark/ast/util/Parsing.java
+++ flexmark/src/main/java/com/vladsch/flexmark/ast/util/Parsing.java
@@ -19,9 +19,9 @@ public class Parsing {
     final public static char INTELLIJ_DUMMY_IDENTIFIER_CHAR = TableFormatOptions.INTELLIJ_DUMMY_IDENTIFIER_CHAR;
     final public static String INTELLIJ_DUMMY_IDENTIFIER = TableFormatOptions.INTELLIJ_DUMMY_IDENTIFIER;
 
-    final public static String NAME_SPACE_START = "[A-Z]|_|[a-z]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u02FF]|[\u0370-\u037D]|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]"; // excluded  [#x10000-#xEFFFF]
-    final public static String NAME_SPACE_CHAR = NAME_SPACE_START + "|-|.|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]";
-//    final public static String NAME_SPACE_START = "[a-z]";
+    final public static String NAME_SPACE_START = "[_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]"; // excluded  [#x10000-#xEFFFF]
+    final public static String NAME_SPACE_CHAR = NAME_SPACE_START + "|[.0-9\u00B7\u0300-\u036F\u203F-\u2040-]";
+//    final public static String NAME_SPACE_START = "[_A-Za-z]";
 //    final public static String NAME_SPACE_CHAR = NAME_SPACE_START + "|-|.|[0-9]";
     final public static String NAME_SPACE = "(?:(?:" + NAME_SPACE_START + ")(?:" + NAME_SPACE_CHAR + ")*:)?";
 
