b2921d6d03c8068d22fe878efcc83ea166430fd4
Thilo-Acrolinx <125662076+Thilo-Acrolinx@users.noreply.github.com>
2024-09-12 10:54:23 +0200

    Remove all JUnit4 test suite classes (#86)

--- flexmark-test-util/src/test/java/com/vladsch/flexmark/test/util/TestUtilsTestSuite.java
+++
@@ -1,8 +0,0 @@
-package com.vladsch.flexmark.test.util;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({LoadUnloadDataKeyAggregatorTest.class, TestUtilsTest.class})
-public class TestUtilsTestSuite {}
